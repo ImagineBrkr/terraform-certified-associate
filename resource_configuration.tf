bfd9a6e81533c37091a588c83052fa9f9379db2b
ImagineBrkr <salvattore_25@hotmail.com>
2025-01-09 12:05:38 -0500

    Resource configuration cont.

--- resource_configuration.tf
+++ resource_configuration.tf
@@ -46,3 +46,118 @@ variable "http_port" {
 # set = ["test-1", "test-2"] (like list but without ordering)
 # map = {name = "Juan", age = 52}
 # null
+
+# Retrieving a value from a map or list:
+
+variable "test_map" {
+  default = {
+    us-east-1 = "ami-123456"
+    us-east-2 = "ami-654321"
+  }
+  type = map
+}
+
+variable "test_list" {
+  default = ["us-east-1", "us-east-2"]
+  type = list
+}
+
+resource "aws_instance" "test_instance" {
+  instance_type = var.test_list[0]
+  ami = var.test_map["us-east-1"]
+}
+
+# COUNT ARGUMENT
+
+# With count we can create multiple resources with the same configuration
+resource "aws_security_group" "multiple_security_groups" {
+  count = 2
+  name = "firewall-${count.index}"
+}
+
+# We can access the resources with aws_security_group.multiple_security_groups[0] and so on
+
+# CONDITIONAL EXPRESSIONS
+
+# We can use conditional expressions to create resources based on a condition
+variable "Environment" {
+  default = "dev"
+}
+
+resource "aws_instance" "conditional_instance" {
+  instance_type = var.Environment == "dev" ? "t2.micro" : "t2.large" # If the condition is true, the first value is used, otherwise the second
+  ami = "ami-123456"
+}
+
+# We can use more complex conditionals with "&&" and "||" operators
+
+# FUNCTIONS
+
+# We can use functions to manipulate data like:
+# max(1,2) -> 2
+# file("file.txt") -> Reads the file
+# min(1,2) -> 1
+# Full docs: https://developer.hashicorp.com/terraform/language/functions
+# Terraform does NOT support user defined functions
+
+# LOCAL VALUES
+
+# We can use local values to store intermediate values
+# The difference with variables is that local values are not exposed to the outside
+
+locals {
+  common_tags = {
+    Name = "test"
+    Environment = var.Environment
+    CreadionDate = formatdate("YYYY-MM-DD", timestamp()) # We can use functions
+  }
+}
+
+resource "aws_instance" "local_instance" {
+  instance_type = "t2.micro"
+  ami = "ami-123456"
+  tags = local.common_tags
+}
+
+resource "aws_security_group" "local_security_group" {
+  tags = local.common_tags
+  name = "firewall-local"
+}
+
+# DATA SOURCES
+
+# Data sources are used to retrieve information from the provider
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
+data "aws_caller_identity" "current" {
+}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id # We can get data from AWS
+}
+
+# For filtering, we need to look at the docs
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ami
+data "aws_ami" "latest" {
+  most_recent = true # We can filter by most recent
+  owners = ["amazon"] # We can filter by owner
+  filter {
+    name = "name" # We can filter by name
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
+  }
+  filter {
+    name = "region"
+    values = ["us-east-1"]
+  }
+}
+
+output "ami_id" {
+  value = data.aws_ami.latest.id
+}
+
+# LOGGING
+
+# For detailed logs, use the TF_LOG environment variable
+# Levels: TRACE, DEBUG, INFO, WARN, ERROR
+# To save the logs, use TF_LOG_PATH environment variable
